4e04ad6c06b3e7a9254e964b49228fa85b9c3361
Kowal <marek.kowal@capgemini.com>
2018-12-19 16:07:48 +0100

    added some code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,64 @@
 
-pub fn add_one(x:i32) -> i32{
-    x + 1
+/// sqrt function
+/// 
+pub fn sqrt(v:f64) -> Option<f64>{
+    match v.sqrt() {
+        x if x.is_normal() => Some(x),
+        _                  => None,
+    }
+}
+
+/// log function
+/// 
+pub fn log(v:f64) -> Option<f64>{
+    match v.log(10.0) {
+        x if x.is_normal() => Some(x),
+        _                  => None,
+    }
+}
+
+///negate function
+///
+pub fn neg(v:f64) -> f64{
+    -1.0 * v
+}
+
+///pow(2) function
+/// 
+pub fn square(v:f32) -> f64{
+    (v as f64).powi(2)
+}
+
+/// double function
+/// 
+pub fn double(v:f32) -> f64{
+    (v as f64) * 2.0
+}
+
+pub fn double64(v:f64) -> f64{
+    v.powi(10)
 }
 
 #[cfg(test)]
 mod utests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn square_maxf32_test() {
+        assert!(square(std::f32::MAX).is_finite());
+    }
+
+    #[test]
+    fn double64_check(){
+        let m = std::f64::MAX;
+        assert!(double64(m).is_infinite());
+    }
+
+    #[test]
+    fn double64_opt_chain(){
+        let m = std::f64::MAX;
+        let res = Some(3.0).map(double64).and_then(|x|{ Some(x) } );
+        assert_eq!(None, res);
     }
 }
